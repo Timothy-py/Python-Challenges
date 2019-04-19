59fe64e087a150b99aa023a0381de8ce0bc193aa
ADEYEYE TIMOTHY <adeyeyetimothy33@gmail.com>
2019-04-19 12:02:58 +0100

    first commit for distinct pair

---
+++ distinct_pair.py
@@ -0,0 +1,32 @@
+def odd(self, product):
+    if product/:
+        return True
+
+sequence = []
+print('Enter sequence of numbers ')
+
+while True:
+    prompt_no = input('Enter a number here: ')
+    if prompt_no != 'stop' or 'STOP':
+        try:
+            int(prompt_no)
+        except ValueError as error:
+            print(error)
+            print('Please enter a positive integer value')
+        else:
+            sequence.append(prompt_no)
+    else:
+        for number in sequence:
+            for n in range(0, sequence[-1]):
+                product = number * n
+                if product is odd():
+                    print('{} * {} = {}'.format(number, n, product))
+                else:
+                    pass
+
+
+
+
+
+
+
